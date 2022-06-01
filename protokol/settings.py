dac1e899b3d4f6d2d9546ab2d6659f2b076efc39
Ilya Abashin <iabashin@getmobit.ru>
2022-06-01 10:17:46 +0300

    Receiver groups, minor refactoring

--- protokol/settings.py
+++ protokol/settings.py
@@ -1,4 +1,9 @@
 import os
 
-DEBUG = os.getenv('DEBUG', None) == 'true'
+
+def get_boolean(key: str) -> bool:
+    return os.getenv(key, '').lower() in ('true', 't', 'yes', 'y', '1')
+
+
+DEBUG = get_boolean('DEBUG')
 CALL_TIMEOUT = 5
