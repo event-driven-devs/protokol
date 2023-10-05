e3d76c5ffaf4d144fc973d5a3aded5d4f8f330ad
Ilya Abashin <iabashin@getmobit.ru>
2023-10-05 11:16:35 +0300

    Added retry config support for request/publish ops

--- protokol/transports/base.py
+++ protokol/transports/base.py
@@ -1,7 +1,15 @@
-from typing import Callable
+from typing import Any, Callable, Optional
+
+from tenacity import AsyncRetrying
+
+
+_sentinel = object()
 
 
 class Transport:
+    def __init__(self, default_retry_config: Optional[AsyncRetrying] = None):
+        self._default_retry_config = default_retry_config
+
     async def connect(self, *urls: str, **kwargs):
         raise NotImplementedError
 
@@ -13,11 +21,47 @@ class Transport:
     ):
         raise NotImplementedError
 
-    async def publish(self, realm, message, **kwargs):
+    async def _do_publish(self, realm: str, message: Any, **kwargs):
         raise NotImplementedError
 
-    async def request(self, realm, message, **kwargs):
+    async def publish(
+        self,
+        realm: str,
+        message: Any,
+        retry_config: Optional[AsyncRetrying] = _sentinel,
+        **kwargs
+    ):
+        if retry_config is _sentinel:
+            retry_config = self._default_retry_config
+
+        if retry_config:
+            async for attempt in retry_config:
+                with attempt:
+                    return await self._do_publish(
+                        realm=realm, message=message, **kwargs
+                    )
+        return await self._do_publish(realm=realm, message=message, **kwargs)
+
+    async def _do_request(self, realm: str, message: Any, **kwargs):
         raise NotImplementedError
+
+    async def request(
+        self,
+        realm: str,
+        message: Any,
+        retry_config: Optional[AsyncRetrying] = _sentinel,
+        **kwargs
+    ):
+        if retry_config is _sentinel:
+            retry_config = self._default_retry_config
+
+        if retry_config:
+            async for attempt in retry_config:
+                with attempt:
+                    return await self._do_request(
+                        realm=realm, message=message, **kwargs
+                    )
+        return await self._do_request(realm=realm, message=message, **kwargs)
 
     async def monitor(self, callback: Callable, **kwargs):
         raise NotImplementedError
